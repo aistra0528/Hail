23015b00706dd8999fd69c3d7a0dd781b87ff180
IcebemAst <2369631276@qq.com>
2022-02-07 19:56:15 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aistra.hail"
         minSdk = 23
         targetSdk = 31
-        versionCode = 10
-        versionName = "0.8.0"
+        versionCode = 11
+        versionName = "0.9.0"
         resourceConfigurations += arrayOf("en", "ja-rJP", "zh-rCN")
     }
     signingConfigs {
