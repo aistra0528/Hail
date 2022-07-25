92aead2e48660bb16ae450a31aa4a9d1930a6211
IcebemAst <2369631276@qq.com>
2022-07-25 17:07:21 +0800

    feat: skip notifying app

--- app/src/main/kotlin/com/aistra/hail/work/AutoFreezeWorker.kt
+++ app/src/main/kotlin/com/aistra/hail/work/AutoFreezeWorker.kt
@@ -6,17 +6,17 @@ import androidx.work.WorkerParameters
 import com.aistra.hail.HailApp
 import com.aistra.hail.app.AppManager
 import com.aistra.hail.app.HailData
+import com.aistra.hail.services.AutoFreezeService
 import com.aistra.hail.utils.HSystem
 
 class AutoFreezeWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
-        if (isSkipWhileCharging(applicationContext)) return Result.success()
+        if (HSystem.isInteractive(applicationContext) || isSkipWhileCharging(applicationContext)) return Result.success()
         var i = 0
         var denied = false
         HailData.checkedList.forEach {
             when {
-                isSkipOnScreenOff(applicationContext, it.packageName)
-                        || AppManager.isAppFrozen(it.packageName) -> return@forEach
+                isSkipApp(applicationContext, it.packageName) -> return@forEach
                 AppManager.setAppFrozen(it.packageName, true) -> i++
                 it.packageName != HailApp.app.packageName && it.applicationInfo != null ->
                     denied = true
@@ -28,7 +28,8 @@ class AutoFreezeWorker(context: Context, params: WorkerParameters) : Worker(cont
     private fun isSkipWhileCharging(context: Context): Boolean =
         HailData.skipWhileCharging && HSystem.isCharging(context)
 
-    private fun isSkipOnScreenOff(context: Context, packageName: String): Boolean =
-        HailData.skipForegroundApp && HSystem.isForegroundApp(context, packageName)
-                && HSystem.isInteractive(context).not()
+    private fun isSkipApp(context: Context, packageName: String): Boolean =
+        AppManager.isAppFrozen(packageName)
+                || (HailData.skipForegroundApp && HSystem.isForegroundApp(context, packageName))
+                || (HailData.skipNotifyingApp && packageName in AutoFreezeService.instance.activeNotifications.map { it.packageName })
 }
