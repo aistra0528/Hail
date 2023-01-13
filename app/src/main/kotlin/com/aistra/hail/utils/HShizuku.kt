0e89ad681bc8426592174dd22f42513f82df46f1
Aistra <55940744+aistra0528@users.noreply.github.com>
2023-01-14 05:49:00 +0800

    perf: Shizuku/Sui

--- app/src/main/kotlin/com/aistra/hail/utils/HShizuku.kt
+++ app/src/main/kotlin/com/aistra/hail/utils/HShizuku.kt
@@ -3,7 +3,6 @@ package com.aistra.hail.utils
 import android.content.pm.PackageManager
 import android.os.IBinder
 import android.os.SystemClock
-import android.system.Os
 import android.view.InputEvent
 import android.view.KeyEvent
 import com.aistra.hail.BuildConfig
@@ -14,30 +13,25 @@ import rikka.shizuku.SystemServiceHelper
 
 object HShizuku {
     private val isRoot get() = Shizuku.getUid() == 0
+    private val userId get() = if (isRoot) android.os.Process.myUserHandle().hashCode() else 0
+    private val callerPackage get() = if (isRoot) BuildConfig.APPLICATION_ID else "com.android.shell"
 
-    private fun asInterface(className: String, serviceName: String): Any? =
-        Class.forName("$className\$Stub")
-            .getMethod("asInterface", IBinder::class.java)
-            .invoke(null, ShizukuBinderWrapper(SystemServiceHelper.getSystemService(serviceName)))
+    private fun asInterface(className: String, serviceName: String): Any =
+        Class.forName("$className\$Stub").getMethod("asInterface", IBinder::class.java)
+            .invoke(null, ShizukuBinderWrapper(SystemServiceHelper.getSystemService(serviceName)))!!
 
     val lockScreen
         get() = try {
-            val proxy = asInterface("android.hardware.input.IInputManager", "input")!!
-            val inject = proxy::class.java.getMethod(
-                "injectInputEvent",
-                InputEvent::class.java,
-                Int::class.java
+            val input = asInterface("android.hardware.input.IInputManager", "input")
+            val inject = input::class.java.getMethod(
+                "injectInputEvent", InputEvent::class.java, Int::class.java
             )
             val now = SystemClock.uptimeMillis()
             inject.invoke(
-                proxy,
-                KeyEvent(now, now, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_POWER, 0),
-                0
+                input, KeyEvent(now, now, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_POWER, 0), 0
             )
             inject.invoke(
-                proxy,
-                KeyEvent(now, now, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_POWER, 0),
-                0
+                input, KeyEvent(now, now, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_POWER, 0), 0
             )
             true
         } catch (t: Throwable) {
@@ -48,24 +42,20 @@ object HShizuku {
     fun setAppDisabledAsUser(packageName: String, disabled: Boolean): Boolean {
         HPackages.getPackageInfoOrNull(packageName) ?: return false
         try {
-            val proxy = asInterface("android.content.pm.IPackageManager", "package")!!
-            proxy::class.java.getMethod(
+            val pm = asInterface("android.content.pm.IPackageManager", "package")
+            val newState = when {
+                !disabled -> PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                isRoot -> PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                else -> PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER
+            }
+            pm::class.java.getMethod(
                 "setApplicationEnabledSetting",
                 String::class.java,
                 Int::class.java,
                 Int::class.java,
                 Int::class.java,
                 String::class.java
-            ).invoke(
-                proxy,
-                packageName,
-                if (!disabled) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-                else if (isRoot) PackageManager.COMPONENT_ENABLED_STATE_DISABLED
-                else PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER,
-                0,
-                Os.getuid() / 100000,
-                BuildConfig.APPLICATION_ID
-            )
+            ).invoke(pm, packageName, newState, 0, userId, callerPackage)
         } catch (t: Throwable) {
             HLog.e(t)
         }
@@ -75,18 +65,18 @@ object HShizuku {
     fun setAppSuspendedAsUser(packageName: String, suspended: Boolean): Boolean {
         HPackages.getPackageInfoOrNull(packageName) ?: return false
         return try {
-            val proxy = asInterface("android.content.pm.IPackageManager", "package")!!
+            val pm = asInterface("android.content.pm.IPackageManager", "package")
             (HiddenApiBypass.invoke(
-                proxy::class.java,
-                proxy,
+                pm::class.java,
+                pm,
                 "setPackagesSuspendedAsUser",
                 arrayOf(packageName),
                 suspended,
                 null,
                 null,
                 null,
-                if (isRoot) BuildConfig.APPLICATION_ID else "com.android.shell",
-                Os.getuid() / 100000
+                callerPackage,
+                userId
             ) as Array<*>).isEmpty()
         } catch (t: Throwable) {
             HLog.e(t)
