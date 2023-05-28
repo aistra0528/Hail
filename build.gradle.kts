46ccfaa92fd076256808cfc835ebbf75c9ccb5c5
Aistra <55940744+aistra0528@users.noreply.github.com>
2023-05-28 12:22:46 +0800

    build: bump AGP version to 8.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.1" apply false
+    id("com.android.application") version "8.0.2" apply false
     kotlin("android") version "1.8.20" apply false
 }
