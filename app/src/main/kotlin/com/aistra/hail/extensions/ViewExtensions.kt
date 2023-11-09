29fc823c5cb570c26c0e420644d7f60b106c0a0f
Jesse205 <2055675594@qq.com>
2023-11-09 20:23:51 +0800

    fix: add origin paddings

--- app/src/main/kotlin/com/aistra/hail/extensions/ViewExtensions.kt
+++ app/src/main/kotlin/com/aistra/hail/extensions/ViewExtensions.kt
@@ -1,6 +1,7 @@
 package com.aistra.hail.extensions
 
 import android.view.View
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePaddingRelative
@@ -16,14 +17,15 @@ fun View.applyInsetsPadding(
     top: Boolean = false,
     bottom: Boolean = false
 ) {
+    val origin = Insets.of(paddingLeft, paddingTop, paddingRight, paddingBottom)
     ViewCompat.setOnApplyWindowInsetsListener(this) { v, windowInsets ->
         val insets =
             windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
         v.updatePaddingRelative(
-            start = if (start) insets.getStart(v) else 0,
-            end = if (end) insets.getEnd(v) else 0,
-            bottom = if (bottom) insets.bottom else 0,
-            top = if (top) insets.top else 0,
+            start = origin.getStart(v) + if (start) insets.getStart(v) else 0,
+            end = origin.getEnd(v) + if (end) insets.getEnd(v) else 0,
+            bottom = origin.bottom + if (bottom) insets.bottom else 0,
+            top = origin.top + if (top) insets.top else 0,
         )
         windowInsets
     }
