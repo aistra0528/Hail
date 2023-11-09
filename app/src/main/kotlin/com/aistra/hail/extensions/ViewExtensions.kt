a2eecc51852d5c603aab4a5ad9c73ddf459f2961
Jesse205 <2055675594@qq.com>
2023-11-09 20:36:01 +0800

    feat: add applyInsetsMargin

--- app/src/main/kotlin/com/aistra/hail/extensions/ViewExtensions.kt
+++ app/src/main/kotlin/com/aistra/hail/extensions/ViewExtensions.kt
@@ -1,9 +1,15 @@
 package com.aistra.hail.extensions
 
 import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsCompat.Type.displayCutout
+import androidx.core.view.WindowInsetsCompat.Type.systemBars
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
 import androidx.core.view.updatePaddingRelative
 
 val View.isRtl get() = layoutDirection == View.LAYOUT_DIRECTION_RTL
@@ -19,14 +25,36 @@ fun View.applyInsetsPadding(
 ) {
     val origin = Insets.of(paddingLeft, paddingTop, paddingRight, paddingBottom)
     ViewCompat.setOnApplyWindowInsetsListener(this) { v, windowInsets ->
-        val insets =
-            windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+        val insets = windowInsets.getInsets(systemBars() or displayCutout())
         v.updatePaddingRelative(
             start = origin.getStart(v) + if (start) insets.getStart(v) else 0,
             end = origin.getEnd(v) + if (end) insets.getEnd(v) else 0,
             bottom = origin.bottom + if (bottom) insets.bottom else 0,
             top = origin.top + if (top) insets.top else 0,
         )
+        windowInsets
+    }
+}
+
+fun View.applyInsetsMargin(
+    start: Boolean = false,
+    end: Boolean = false,
+    top: Boolean = false,
+    bottom: Boolean = false
+) {
+    val origin = Insets.of(marginLeft, marginTop, marginRight, marginBottom)
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, windowInsets ->
+        val insets = windowInsets.getInsets(systemBars() or displayCutout())
+        (v.layoutParams as MarginLayoutParams).apply {
+            setMargins(
+                0,
+                origin.top + if (top) insets.top else 0,
+                0,
+                origin.bottom + if (bottom) insets.bottom else 0
+            )
+            marginStart = origin.getStart(v) + if (start) insets.getStart(v) else 0
+            marginEnd = origin.getEnd(v) + if (end) insets.getEnd(v) else 0
+        }
         windowInsets
     }
 }
