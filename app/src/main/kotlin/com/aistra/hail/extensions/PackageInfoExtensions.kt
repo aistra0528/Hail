d6eafb687ecdd53694ff5127dd928b648184a8e6
Jesse205 <2055675594@qq.com>
2023-11-23 22:40:50 +0800

    feat: select export apk dir

---
+++ app/src/main/kotlin/com/aistra/hail/extensions/PackageInfoExtensions.kt
@@ -0,0 +1,15 @@
+package com.aistra.hail.extensions
+
+import android.content.pm.PackageInfo
+import androidx.core.content.pm.PackageInfoCompat
+
+import com.aistra.hail.HailApp.Companion.app
+
+/**
+ * The name of the file when exported to sd card, in the format `AppName-VersionName-VersionNumber.apk`.
+ * */
+val PackageInfo.exportFileName
+    get() = "${applicationInfo.loadLabel(app.packageManager)}-${versionName ?: "unknown"}-${
+        PackageInfoCompat.getLongVersionCode(this)
+    }.apk"
+
