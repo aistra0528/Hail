d8d98ef4f4a613858c2fefc689be70003a1c79b6
Aistra <aistra0528@foxmail.com>
2024-06-18 17:59:19 +0800

    build: bump kotlin android plugin version to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    kotlin("android") version "1.9.20" apply false
+    kotlin("android") version "2.0.0" apply false
 }
