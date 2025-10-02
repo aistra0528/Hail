aa2545b0912d7b594649023416f979ea5612c0ed
Aistra <aistra0528@foxmail.com>
2025-10-02 11:56:28 +0800

    fix: restore background optimization #339

--- app/src/main/kotlin/com/aistra/hail/receiver/UnsuspendedReceiver.kt
+++ app/src/main/kotlin/com/aistra/hail/receiver/UnsuspendedReceiver.kt
@@ -4,10 +4,16 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.aistra.hail.HailApp.Companion.app
+import com.aistra.hail.utils.HShizuku.setAppRestricted
+import com.aistra.hail.utils.HTarget
 
 class UnsuspendedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == ACTION_PACKAGE_UNSUSPENDED_MANUALLY) runCatching {
+            if (HTarget.P) setAppRestricted(
+                intent.getStringExtra(Intent.EXTRA_PACKAGE_NAME)!!,
+                false
+            )
             app.setAutoFreezeService()
         }
     }
